5d5e5419bee4b022ae5c4096905ef902f5a682f4
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-06-13 01:14:51 +0400

    feat:CRUD

---
+++ EduHome/Areas/AdminF/Controllers/NoticeRightInfoController.cs
@@ -0,0 +1,45 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.AdminF.Controllers
+{
+    [Area("Adminf")]
+    public class NoticeRightInfoController : Controller
+    {
+        private AppDbContext _context;
+        public NoticeRightInfoController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            List<NoticeRightInfo> noticeRightInfos = _context.NoticeRightInfos.ToList();
+
+            return View(noticeRightInfos);
+            
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            NoticeRightInfo dbnoticeRightInfo = await _context.NoticeRightInfos.FindAsync(id);
+            if (dbnoticeRightInfo == null) return NotFound();
+            return View(dbnoticeRightInfo);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            NoticeRightInfo dbnoticeRightInfo = await _context.NoticeRightInfos.FindAsync(id);
+            if (dbnoticeRightInfo == null) return NotFound();
+            _context.NoticeRightInfos.Remove(dbnoticeRightInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ EduHome/Areas/AdminF/Controllers/UpCommingEventController.cs
@@ -0,0 +1,43 @@
+﻿using EduHome.DAL;
+using EduHome.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Areas.AdminF.Controllers
+{
+    [Area("AdminF")]
+    public class UpCommingEventController : Controller
+    {
+        private AppDbContext _context;
+        public UpCommingEventController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<UpCommingEvent> dbUpCommingEvents = _context.UpCommingEvents.ToList();
+            return View(dbUpCommingEvents);
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return NotFound();
+
+            UpCommingEvent dbUpCommingEvent =await  _context.UpCommingEvents.FindAsync(id);
+            if (dbUpCommingEvent == null) return NotFound();
+            return View(dbUpCommingEvent);
+
+        }
+        public async Task<IActionResult>Delete(int? id)
+        {
+            if (id == null) return NotFound();
+            UpCommingEvent dbUpCommingEvent = await _context.UpCommingEvents.FindAsync(id);
+            if (dbUpCommingEvent == null) return NotFound();
+            _context.UpCommingEvents.Remove(dbUpCommingEvent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
